148c5760723955563fe35d50f9cdc3fb107b69d4
sarikakuggle <krishnareddykovvuri@Krishnas-Mac-mini-2.local>
2015-11-13 12:12:48 +0530

    Adding Buttons

---
+++ pageExample/pageExample/ButtonViewController.h
@@ -0,0 +1,13 @@
+//
+//  ButtonViewController.h
+//  pageExample
+//
+//  Created by Krishna Reddy Kovvuri on 11/13/15.
+//  Copyright © 2015 Krify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ButtonViewController : UIViewController
+
+@end
